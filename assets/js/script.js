5ecb5ca1cb06ce60cbe9e83d4025d26482d9f1f1
augusto-dmh <augustodemeloh@protonmail.com>
2023-07-26 16:40:47 -0300

    fix : correções no algorimo base
    A lógica do algoritmo foi alterada: havia erros na função getCpfSum e getCheckDigit:
    
    o resultado retornado por getCpfSum estava dando errado por conta da não-utilização do 'regressiveCounter', da não-definição de um initialValue ao usar o método reduce e da conversão do cpf em número - que acarretaria resultados inesperados em cpfs começados com o digito 0. Já em getCheckDigit, apenas estava faltando, caso o resultado da expressão "11 - cpfSum % 11" fosse maior que 9, o digito a ser retornado ser 0.
    
    De resto, o restante das funções foi linkado entre si e agora somente é necessário utilizar "checkCpf" para obtermos o resultado desejado. Isso facilitará na futura integração entre o JS e o input do usuário do HTML do site.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,53 +4,62 @@
 /*the cpf to be received here will be a string, don't forget it.*/
 /*do not forget to integrate all functions to work together and let 'em fine after finishing the project*/
 /*remember: there's no hoisting in arrow functions.*/
-function cpf(cpfUnformatted) {
 
-    /*get cpf without "." and "-"*/
-    const formatCpf = () => {
-        let formattedCpf = cpfUnformatted.replace(/\D+/g, '');
-        return formattedCpf;
-    }
-
-    const getCpfNumber = formattedCpf => {
-        return Number(formattedCpf);
-    }
-
-    /*root = first nine digits*/
-    const getCpfRoot = cpf => {
-        return cpf.slice(0, -2);
-    }
-
-    /*function to get the counter that's gonna be used to get the sum of all root digits - it'll be used to get a check digit*/
-    const getRegressiveCounter = cpf => {
-        if (cpf.length > 9) return 11; /*if the first checkDigit is already on the cpf passed as argument*/
-        return 10;
-    }
-
-    const getCpfSum = cpf => {
-        cpf = getCpfNumber(cpf);
-        return Array.from(cpf).reduce((acc, digit => {
-            return acc + digit;
-        }));
-    }
-
-    const getCheckDigit = cpf => {
-        const regressiveCounter = getRegressiveCounter(cpf);
-        const cpfSum = getCpfSum(cpf);
-        return 11 - cpfSum % 11;
-    }
-
-    const getCpfToCompare = (cpfRoot, ...checkDigits) => {
-        const [checkD1, checkD2] = checkDigits;
-        return cpfRoot + checkD1 + checkD2;
-    }
-
-    const isSequence = cpf => {
-        return cpf === cpf[0].repeat(11);
-    }
-
-    const checkCpf = (cpfReceived, cpfToCompare) => {
-        if (isSequence(cpfReceived)) return false;
-        return cpfReceived === cpfToCompare;
-    }
+/*get cpf without "." and "-"*/
+const formatCpf = cpf => {
+    let formattedCpf = cpf.replace(/\D+/g, '');
+    return formattedCpf;
 }
+
+/*root = first nine digits*/
+const getCpfRoot = cpf => {
+    return cpf.slice(0, -2);
+}
+
+/*function to get the counter that's gonna be used to get the sum of all root digits - it'll be used to get a check digit*/
+const getRegressiveCounter = cpf => {
+    if (cpf.length > 9) return 11; /*if the first checkDigit is already on the cpf passed as argument*/
+    return 10;
+}
+
+const getCpfSum = (cpf, regressiveCounter) => {
+    return Array.from(cpf).reduce((acc, digit) => {
+        digit = Number(digit);
+        acc += regressiveCounter * digit;
+        regressiveCounter--;
+        return acc;
+    }, 0);
+}
+
+const getCheckDigit = cpf => {
+    const regressiveCounter = getRegressiveCounter(cpf); /*it isn't being used anywhere!*/
+    const cpfSum = getCpfSum(cpf, regressiveCounter);
+    const checkDigit = 11 - cpfSum % 11;
+    if (checkDigit > 9) return 0;
+    return checkDigit;
+}
+
+const getCpfToCompare = (cpfRoot, ...checkDigits) => {
+    const [checkD1, checkD2] = checkDigits;
+    return cpfRoot + checkD1 + checkD2;
+}
+
+const isSequence = cpf => {
+    return cpf === cpf[0].repeat(11);
+}
+
+const checkCpf = cpfReceived => {
+    cpfReceived = formatCpf(cpfReceived);
+
+    if (isSequence(cpfReceived)) return false;
+
+    const cpfRoot = getCpfRoot(cpfReceived);
+    const checkD1 = getCheckDigit(cpfRoot);
+    const checkD2 = getCheckDigit(cpfRoot + checkD1);
+    const cpfToCompare = getCpfToCompare(cpfRoot, checkD1, checkD2);
+
+    return cpfReceived === cpfToCompare;
+}
+
+const cpf = '081.683.130-03';
+console.log(checkCpf(cpf));
