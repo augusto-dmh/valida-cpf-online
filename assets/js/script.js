48d7d881813bea2e794b27460ff34fdb8b043025
augusto-dmh <augustodemeloh@protonmail.com>
2023-08-09 14:12:59 -0300

    feat/refactor: JS gerar cpf + renaming
    Foi implementada a funcionalidade de geração de CPF no script. Para incluí-la, tiveram de ser feitas alterações em nomes de variáveis e funções a fim de se garantir uma melhor legibilidade ao código.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,14 @@
 /*do not forget to integrate all functions to work together and let 'em fine after finishing the project*/
 /*remember: there's no hoisting in arrow functions.*/
 
-const input = document.querySelector('#cpf-input');
+const inputCheck = document.querySelector('.check-cpf-input');
 const form = document.querySelector('form');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 });
 
-(function cpfValidation() {
+(function cpfValidation_Generation() {
 
     // SHOW CPF VALIDATION RESULT //
     const containsAnyClass = (element, classList) => classList.some(className => element.classList.contains(className));
@@ -40,7 +40,7 @@ form.addEventListener('submit', e => {
 
 
     // CPF VALIDATION //
-    const formatCpf = cpf => cpf.replace(/\D+/g, ''); /*get cpf without "." and "-"*/
+    const getCleanCpf = cpf => cpf.replace(/\D+/g, ''); /*get cpf without "." and "-"*/
 
     const getCpfRoot = cpf => cpf.slice(0, -2); /*root = first nine digits*/
 
@@ -72,7 +72,7 @@ form.addEventListener('submit', e => {
     const isSequence = cpf => cpf === cpf[0].repeat(11);
 
     const checkCpf = cpfReceived => {
-        cpfReceived = formatCpf(cpfReceived);
+        cpfReceived = getCleanCpf(cpfReceived);
         const cpfToCompare = getCpfToCompare(cpfReceived);
 
         if (isSequence(cpfReceived)) return toggleValidationMsg(false);
@@ -81,14 +81,46 @@ form.addEventListener('submit', e => {
     }
 
     document.addEventListener('keyup', e => {
-        if (e.key === 'Enter') checkCpf(input.value);
+        if (e.key === 'Enter') checkCpf(inputCheck.value);
     });
+
+
+    //CPF GENERATION
+    const inputGen = document.querySelector('.gen-cpf-input');
+    const genBtn = document.querySelector('.gen-cpf-btn');
+
+    const getRandomDigit = () => Math.floor(Math.random() * 10);
+
+    const getRandomCpfRoot = () => Array.from({ length: 9 }, getRandomDigit).join('');
+
+    const getRandomCpf = () => {
+        const cpfRoot = getRandomCpfRoot();
+        const checkD1 = getCheckDigit(cpfRoot);
+        const checkD2 = getCheckDigit(cpfRoot + checkD1);
+
+        return cpfRoot + checkD1 + checkD2;
+    }
+
+    const getCpfFormatted = cpf => {
+        return (
+            cpf.slice(0, 3) + '.' +
+            cpf.slice(3, 6) + '.' +
+            cpf.slice(6, 9) + '-' +
+            cpf.slice(9, 12)
+        );
+    }
+
+    genBtn.addEventListener('click', () => {
+        const randomCpf = getRandomCpf();
+        inputGen.value = getCpfFormatted(randomCpf);
+    })
+
 })();
 
-(function cpfInput() {
+(function cpfValidationInput() {
 
     const cpfMask = () => {
-        input.value = input.value
+        inputCheck.value = inputCheck.value
             .replace(/\D/g, '')
             .replace(/(\d{3})(\d)/, '$1.$2')
             .replace(/(\d{3})(\d)/, '$1.$2')
@@ -96,13 +128,13 @@ form.addEventListener('submit', e => {
             .replace(/(-\d{2})\d+?$/, '$1');
     }
 
-    const removeLastChar = () => input.value = input.value.slice(0, -1);
+    const removeLastChar = () => inputCheck.value = inputCheck.value.slice(0, -1);
 
     const checkInput = () => {
-        if (!input.value.slice(-1).match(/\d+/g)) removeLastChar(); /*if the last character is not a digit, immediately remove it from the input*/
-        if (input.value.length > 14) removeLastChar(); /*if the last digit makes the cpfInput.value greater than 14 (11 digits + 4 specialChars), immediately remove it from the input*/
+        if (!inputCheck.value.slice(-1).match(/\d+/g)) removeLastChar(); /*if the last character is not a digit, immediately remove it from the input*/
+        if (inputCheck.value.length > 14) removeLastChar(); /*if the last digit makes the cpfInput.value greater than 14 (11 digits + 4 specialChars), immediately remove it from the input*/
     }
 
-    input.addEventListener('input', checkInput);
-    input.addEventListener('keydown', cpfMask);
+    inputCheck.addEventListener('input', checkInput);
+    inputCheck.addEventListener('keydown', cpfMask);
 })();
